93719e11ab1182446f1ae34f579feb60fa9d1458
lysxria1134 <sqeak65@gmail.com>
2026-04-06 19:19:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const uiManager = {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    model: 'tinyllama',
+                    model: 'llama3',
                     prompt: text,
                     stream: false
                 })
